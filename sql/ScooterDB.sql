2c3b77fb1f8968740382557c35783857df7ce0ed
Claudio <cvertemara@csumb.edu>
2020-04-21 14:45:54 -0700

    Added DB Info

--- sql/ScooterDB.sql
+++ sql/ScooterDB.sql
@@ -31,13 +31,26 @@ CREATE TABLE `locations` (
 --
 
 INSERT INTO `locations` (`id`, `name`, `numOfDevices`, `api`) VALUES
-(1, 'Administration Building', '4', 'Sunny'),
-(2, 'Agatha', 'Christie', '1890-09-15'),
-(3, 'Marie', 'Curie', '1867-11-07'),
-(4, 'Abraham', 'Lincoln', '1809-02-12'),
-(5, 'Mohandas', 'Gandhi', '1869-10-02'),
-(6, 'Helen', 'Keller', '1880-06-27'),
-(7, 'Benjamin', 'Franklin', '1706-01-17');
+(1, 'Administration Building', 4, 'Sunny'),
+(2, 'Alumni & Visitors Center', 8, 'Sunny'),
+(3, 'Beach Hall', 13, 'Sunny'),
+(4, 'Black Box Cabaret', 18, 'Sunny'),
+(5, 'Chapman Science Academic Center', 16, 'Sunny'),
+(6, 'College of Arts, Humanities, and Social Sciences', 4, 'Sunny'),
+(7, 'Cinematic Arts & Technology', 9, 'Sunny');
+(8, 'Coast Hall', 2, 'Sunny');
+(9, 'Del Mar', 20, 'Sunny');
+(10, 'Dining Commons', 3, 'Sunny');
+(11, 'Dunes Hall', 5, 'Sunny');
+(12, 'Facilities Services & Operations', 6, 'Sunny');
+(13, 'Gambord Business Information & Technology', 11, 'Sunny');
+(14, 'Gavilan Hall', 1, 'Sunny');
+(15, 'Green Hall', 5, 'Sunny');
+(16, 'Harbor Hall', 4, 'Sunny');
+(17, 'Manzanita Hall', 8, 'Sunny');
+(18, 'Promontory – West', 12, 'Sunny');
+(19, 'Promontory – Center', 15, 'Sunny');
+(20, 'Vineyard Suites', 1, 'Sunny');
 
 LOCK TABLES `locations` WRITE;
 /*!40000 ALTER TABLE `locations` DISABLE KEYS */;
@@ -91,13 +104,26 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `age`, `password`) VALUES
-(1, 'Albert', 'Einstein', 'email@email.com', 0, 'pass'),
-(2, 'Agatha', 'Christie', 'email@email.com', 0, 'pass'),
-(3, 'Marie', 'Curie', 'email@email.com', 0, 'pass'),
-(4, 'Abraham', 'Lincoln', 'email@email.com', 0, 'pass'),
-(5, 'Mohandas', 'Gandhi', 'email@email.com', 0, 'pass'),
-(6, 'Helen', 'Keller', 'email@email.com', 0, 'pass'),
-(7, 'Benjamin', 'Franklin', 'email@email.com', 0, 'pass');
+(1, 'Geoffrey Delgado', 'gdun', 'email@email.com', 42, 'gdpass'),
+(2, 'Nick Patterson', 'npun', 'email@email.com', 15, 'nppass'),
+(3, 'Simon Ward', 'swun', 'email@email.com', 31, 'swpass'),
+(4, 'Kristy Lucas', 'klun', 'email@email.com', 99, 'klpass'),
+(5, 'Craig Miller', 'cmun', 'email@email.com', 23, 'cmpass'),
+(6, 'Joanne Barton', 'jbun', 'email@email.com', 42, 'jbpass'),
+(7, 'Helen Sanders', 'hsun', 'email@email.com', 50, 'hspass');
+(8, 'Jimmy Tucker', 'jtun', 'email@email.com', 18, 'jtpass');
+(9, 'Sara Vega', 'svun', 'email@email.com', 76, 'svpass');
+(10, 'Vincent Rowe', 'vrun', 'email@email.com', 19, 'vrpass');
+(11, 'Madeline Ramsey', 'mrun', 'email@email.com', 34, 'mrpass');
+(12, 'Bradford Moody', 'bmun', 'email@email.com', 46, 'bmpass');
+(13, 'Billie Walker', 'bwun', 'email@email.com', 23, 'bwpass');
+(14, 'Richard Wilkerson', 'rwun', 'email@email.com', 27, 'rwpass');
+(15, 'Stacy Lewis', 'slun', 'email@email.com', 67, 'slpass');
+(16, 'Ginger Mitchell', 'gmun', 'email@email.com', 43, 'gmpass');
+(17, 'Felix Webster', 'fwun', 'email@email.com', 22, 'fwpass');
+(18, 'Miriam Hansen', 'mhun', 'email@email.com', 33, 'mhpass');
+(19, 'June Flores', 'jfun', 'email@email.com', 88, 'jfpass');
+(20, 'Derek Reese', 'drun', 'email@email.com', 17, 'drpass');
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
@@ -135,19 +161,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `l9_quotes`
 --
 ALTER TABLE `pricing`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
   
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `users`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
